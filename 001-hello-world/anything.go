c0939fbd4581ac9d4f0ecf9355b89e3857f370fc
prateekv12 <prateekv12@users.noreply.github.com>
2020-05-12 19:48:08 -0400

    Control Flow Demo

--- 001-hello-world/anything.go
+++ 001-hello-world/anything.go
@@ -5,6 +5,13 @@ import "fmt"
 func main() {
 	fmt.Println("lets go! Vamos!")
 	foo()
+	fmt.Println("printing something after foo")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
 }
 
 func foo() {
