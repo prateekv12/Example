ec0a9de720ca840b5f3619bba0957523b0a68b04
prateekv12 <prateekv12@users.noreply.github.com>
2020-05-26 19:28:22 -0400

    fmt package

---
+++ 006-fmt-package/fmt.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+var y = 42
+
+//format printing
+
+func main() {
+
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
+	fmt.Printf("%b\n", y)
+	fmt.Printf("%x\n", y)
+	fmt.Printf("%#x\n", y)
+
+	y = 911
+	fmt.Printf("%#x\n", y)
+	fmt.Printf("%#x\t%b\t%x\n", y, y, y)
+
+	s:= fmt.Sprintf("%#x\t%b\t%x\n", y, y, y)
+	fmt.Println(s)
+}
