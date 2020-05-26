ccb9f930d7a990c6a171bb27defb77cb6ed12021
prateekv12 <prateekv12@users.noreply.github.com>
2020-05-26 19:02:45 -0400

    zero value addition

--- 005-zero-value/zero_value.go
+++ 005-zero-value/zero_value.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 var y string
+var z int
 
 func main() {
 
 	fmt.Println("the value of y is ", y, "end")
-	fmt.Printf("%T", y)
+	fmt.Printf("%T\n", y)
+	y = "Bond, James Bond"
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
+
+	fmt.Println(z)
+	fmt.Printf("%T\n", z)
+
 }
