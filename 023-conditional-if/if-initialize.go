25d6b311d4bf7915fb28c8635223d1dae205c556
prateekv12 <prateekv12@users.noreply.github.com>
2020-05-30 17:55:52 -0400

    if condition

---
+++ 023-conditional-if/if-initialize.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	if x := 42; x == 42 { // initializing the value of x to just the if condition
+		fmt.Println("001")
+	}
+	//fmt.Println("Here is a statement")
+	//fmt.Println("something else")
+
+}
