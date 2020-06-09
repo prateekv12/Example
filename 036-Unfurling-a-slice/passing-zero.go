e3715ec764dfe970bc63f2560b785239d500ee7a
prateekv12 <prateekv12@users.noreply.github.com>
2020-06-08 20:18:08 -0400

    Unfurling a slice

---
+++ 036-Unfurling-a-slice/passing-zero.go
@@ -0,0 +1,27 @@
+//You can create a func which takes an unlimited number of arguments. When you do this, this is known as a “variadic parameter.”
+
+package main
+
+import "fmt"
+
+func main() {
+
+	//xi := []int{2, 3, 4, 5, 6, 7, 8, 9}
+	x := sum() //passing nothing. - VARIADIC means zero or more
+	fmt.Println("the total is ", x)
+}
+
+func sum(x ...int) int {
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+
+	sum := 0
+
+	for i, v := range x {
+		sum += v
+		fmt.Println("for item in index position", i, "we are adding", v, "to the total which is now", sum)
+	}
+
+	//fmt.Println("the total is ", sum)
+	return sum
+}
